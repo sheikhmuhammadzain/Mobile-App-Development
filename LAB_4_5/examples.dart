27ba735feb38c1b54fbcb93fa1845a8d762259c2
sheikhmuhammadzain <zaingee320@gmail.com>
2024-10-01 09:59:55 +0500

    uploading th e code

---
+++ LAB_4_5/examples.dart
@@ -0,0 +1,65 @@
+// import 'dart:io'  ;
+// void main (){
+//   print ('Enter Your Name ');
+//   String ? name = stdin.readLineSync();;
+//   print ('Your Name is $name');
+// }
+
+
+
+// import 'dart:io';
+
+// void main() {
+//   print('Enter Your Roll No');
+//   int?number = int.parse(stdin.readLineSync()!);
+//   print('Your Roll No is $number');
+// }
+
+
+
+// void main (){
+//   List l = List.filled(3, null);
+//   l[0] = 10;
+//   l[1] = 20;
+//   l[2] = 30;
+//   print(l);
+//   print(l[0]);
+// }
+
+
+
+// import 'dart:io';
+
+// void main() {
+//   dynamic customMap= new Map ();
+
+// customMap['zain'] = '20';
+// customMap['ali'] = '21';  
+// print(customMap);
+
+// }
+
+
+// void main (){
+//   double n = 8;
+//   int t = 7;
+//   num s = 9 ;
+//   print (n.hashCode);
+//   print (t.isFinite);
+//   print(identityHashCode(s));
+//   double result = 5/0;
+//   print(result);
+//   print(result.isInfinite);
+//   double nanResult = 0/0;
+//   print(nanResult);
+//   print(nanResult.isNaN);
+//   int g = 5 ;
+//   print(g.bitLength);
+//   print(g.isOdd);
+//   print(g.sign);
+//   print(g.isNegative);
+//   print(g.isEven);
+// }
+
+
+
