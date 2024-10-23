c6e98f7c6ae2df2591ad05de7ff6d6b905e0be34
70132240 <70132240@uol.pk>
2024-10-23 10:00:06 +0500

    sdfsdfsdfsdsdf sfsa

--- currency_converter_material_page.dart
+++ currency_converter_material_page.dart
@@ -7,30 +7,39 @@ class Build extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: 'Enter text here',
-            prefixIcon: Icon(Icons.monetization_on),
-            filled: true,
-            fillColor: Colors.grey[200],
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blue, width: 2.0),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.green, width: 2.0),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.red, width: 2.0),
-              borderRadius: BorderRadius.circular(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter text here',
+                prefixIcon: Icon(Icons.monetization_on),
+                filled: true,
+                fillColor: Colors.grey[200],
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green, width: 2.0),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red, width: 2.0),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              style: TextStyle(fontSize: 18.0),
             ),
           ),
-          keyboardType: TextInputType.numberWithOptions(decimal: true),
-          style: TextStyle(fontSize: 18.0),
-        ),
+          TextButton(
+            onPressed: () {},
+            child: Text('Convert'),
+          ),
+        ],
       ),
     );
   }
