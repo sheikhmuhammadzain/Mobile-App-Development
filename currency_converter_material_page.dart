56ea77574c22d6c74b6b22619580a32712691bd3
70132240 <70132240@uol.pk>
2024-10-23 09:54:17 +0500

    sdfsdfsdfsd

---
+++ currency_converter_material_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Build extends StatelessWidget {
+  const Build({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Enter text here',
+            prefixIcon: Icon(Icons.monetization_on),
+            filled: true,
+            fillColor: Colors.grey[200],
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blue, width: 2.0),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.green, width: 2.0),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red, width: 2.0),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+          keyboardType: TextInputType.numberWithOptions(decimal: true),
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Zain Custom Widget Example')),
+      body: Center(child: Build()), // Using the custom widget
+    ),
+  ));
+}
